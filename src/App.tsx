ca49e366553ba00d41b0f4d81defc561b2533238
nataliayuk <natalia.yuk@icloud.com>
2025-05-12 14:18:43 -0300

    adaptar para vercel

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'  
 import Home from './pages/Home'
 import About from './pages/About'
-import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 import './App.css'
 
